f3110e60f6eaa2a92a2b70d18e777e94a04b3c38
Ndèye aminata mbengue <Ndye aminata mbengue>
2016-06-27 17:57:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/aminatambengue/helloamina/MainActivityHelloAmina.java
@@ -0,0 +1,13 @@
+package com.example.aminatambengue.helloamina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityHelloAmina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hello_amina);
+    }
+}
